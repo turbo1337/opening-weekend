71f79dcb1bf4c4e04cf9646b1d24e36a50018036
turbo1337 <torbam.12d@acsjszki.hu>
2022-02-02 10:31:33 +0100

    1

--- opening-weekend/Program.cs
+++ opening-weekend/Program.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace opening_weekend
 {
+    class Film
+    {
+        public string eredeticim { get; set; }
+        public string magyarcim { get; set; }
+        public string bemutato { get; set; }
+        public string forgalmazo { get; set; }
+        public int bevel { get; set; }
+        public int latogato { get; set; }
+
+        public Film(string sor)
+        {
+            string[] t = sor.Split(';');
+            eredeticim = t[0];
+            magyarcim = t[1];
+            bemutato = t[2];
+            forgalmazo = t[3];
+            bevel = int.Parse(t[4]);
+            latogato = int.Parse(t[5]);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            
+
+
+            Console.ReadKey();
         }
     }
 }
